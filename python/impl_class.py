161fb5e4dd46f9828a54e76390f2478f0173c683
Kelvin Zimmerman <zimtech0@gmail.com>
2021-06-21 14:53:41 -0400

    updates

--- python/impl_class.py
+++ python/impl_class.py
@@ -13,22 +13,27 @@ class Dog:
         self.speakText = speakText
 
 
-#create function to capture speak
+#create assessor method that returns the speakText stored in the object.
     def speak(self):
         return self.speakText
     
-#create function to get name
+#create assessor method to get name
     def get_name(self):
         return self.name
 
-#create function for bday
+#create assessor method that uses bday info
     def birth_date(self):
         return str(self.month) + "/" + str(self.day) + "/"+ str(self.year)
+
 #create function for change speak
-    def change_speak(self,bark):
+    def change_speak(self,bark): 
         self.speakText =  bark
 
 #create a new puppy
+    def __add__(self,other_dog):
+        return Dog("Puppy of " + self.name + " and " + other_dog.name, 
+            self.month, self.day, self.year + 1, \
+            self.speakText + other_dog.speakText) 
         
 
 #create main function
@@ -37,9 +42,18 @@ def main():
     girl_dog = Dog("Sequoia",5,6,2004, "barkbark")
     print(boy_dog.speak())
     print(girl_dog.speak())
+    print(boy_dog.birth_date())
+    print(girl_dog.birth_date())
+    boy_dog.change_speak("woofywooofy")
+    print(boy_dog.speak())
+    puppy = boy_dog + girl_dog
+    print(puppy.speak())
+    print(puppy.get_name())
+    print(puppy.birth_date())
+
     
 
 
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     main()
