b5287ea05be110d1aca617d1beaeb7b686e97109
Kelvin Zimmerman <zimtech0@gmail.com>
2021-06-30 22:40:39 -0400

    updates

--- php/api_php_mysql_xmple/src/vendor/bootstrap.php
+++ php/api_php_mysql_xmple/src/vendor/bootstrap.php
@@ -3,9 +3,10 @@
 require 'vendor/autoload.php';
 use Dotenv\Dotenv;
 
+use Src\System\DatabaseConnector;
 $dotenv = new DotEnv(__DIR__);
 $dotenv->load();
 //test code should output
 //api:default
 //when you run $ php bootstrap.php
-echo getenv('OKTAAUDIENCE');
+$dbConnection = (new DatabaseConnector())->getConnection();
