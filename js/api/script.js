fdd88c18cece890bc479b989c19d9c69a9436205
Kelvin Zimmerman <zimtech0@gmail.com>
2021-07-02 01:43:02 -0400

    updates

--- js/api/script.js
+++ js/api/script.js
@@ -1,6 +1,5 @@
 function fetchData() {
-   // fetch("https://reqres.in/api/users")
-   fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+   fetch("https://jsonplaceholder.typicode.com/users/1/posts")
         .then(response =>{
          if (!response.ok){
              throw Error("ERROR");
@@ -12,10 +11,14 @@ function fetchData() {
          })
          .then(data => {
             console.log(data.data);
-             .map(user =>{
+             .map(userId =>{
                  return `
-                 <div class="user">
-                   <p>Name: ${user.first_name}</p>
+                 <div class="userId">
+                   <p>User Id: ${userId.userId}</p>
+                   <p>Post Id: ${userId.id}</p>
+                   <p>Post Title: ${userId.title}</p>
+                   <p>Post Contents: ${userId.body}</p>
+
                 </div>
                    `;
              })
