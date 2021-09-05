694b439c051b4d99c9a61ccf4ae834c72c7a692a
Kelvin Zimmerman <zimtech0@gmail.com>
2021-09-05 19:13:18 -0400

    updates

---
+++ fearFreePets/functions.php
@@ -0,0 +1,9 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( 'parenthandle' ), 
+        wp_get_theme()->get('Version') // should be v1.0
+    );
+}
+?>
